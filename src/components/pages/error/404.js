c6c3f8a0fa804838c56f89e524ecb683192d5a81
Nasir Mehmood <oknasir@gmail.com>
2021-10-27 22:11:18 +0500

    history maintain

--- src/components/pages/error/404.js
+++ src/components/pages/error/404.js
@@ -1,19 +1,25 @@
 import {Button} from "react-bootstrap";
+import {useHistory} from "react-router-dom"
 
 function NotFound() {
-    return (
-      <div className="d-flex justify-content-center align-items-center vh-100">
-        <div className="text-center">
-          <div className="position-relative">
-            <h1 className="display-xxl m-0">OOPS!</h1>
-            <p className="position-absolute bottom-0 start-50 translate-middle-x text-nowrap d-inline-block display-5 mb-0 bg-white py-1 px-3">404 - The page can't be found</p>
-          </div>
-
-          <Button variant="orange" className="mt-4">Go To Homepage</Button>
+  const history = useHistory();
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="text-center">
+        <div className="position-relative">
+          <h1 className="display-xxl m-0">OOPS!</h1>
+          <p className="position-absolute bottom-0 start-50 translate-middle-x text-nowrap d-inline-block display-5 mb-0 bg-white py-1 px-3">404 - The page can't be found</p>
         </div>
+
+        <Button onClick={(e) => {
+          e.preventDefault()
+
+          history.push('/')
+        }} variant="orange" className="mt-4">Go To Homepage</Button>
       </div>
-    );
-  }
-  
-  export default NotFound;
+    </div>
+  );
+}
+
+export default NotFound;
   
