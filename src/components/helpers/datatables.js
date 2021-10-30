d7e1ba647d222d8c71044df550e19fc0a32da42d
Nasir Mehmood <oknasir@gmail.com>
2021-10-30 22:17:39 +0500

    map loop practice

--- src/components/helpers/datatables.js
+++ src/components/helpers/datatables.js
@@ -6,6 +6,7 @@ export const DataTable = ({data}) => (
             <th scope="col">First</th>
             <th scope="col">Last</th>
             <th scope="col">Handle</th>
+            <th scope="col">Name</th>
             </tr>
         </thead>
         <tbody>
@@ -15,6 +16,7 @@ export const DataTable = ({data}) => (
                     <td>{item.first_name}</td>
                     <td>{item.last_name}</td>
                     <td>{item.username}</td>
+                    <td>{item.name}</td>
                 </tr>
             ))}
         </tbody>
--- src/components/pages/application-form/ViewForm.js
+++ src/components/pages/application-form/ViewForm.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 
-import {Container} from 'react-bootstrap';
+import {Container, Button} from 'react-bootstrap';
 
 import {Pagination, DataTable} from '../../helpers'
 import NavigationBar from "../../navs/NavigationBar"
 
 function ViewForm() {
   const [applications, setApplications] = useState([])
+  const [results, setResults] = useState(null)
 
   useEffect(()=>{
     fetchData()
@@ -14,42 +15,94 @@ function ViewForm() {
 
   const fetchData = () => {
     const data = [
-      {id: 1, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 2, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 3, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 4, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 5, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 6, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 7, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 8, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 9, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 10, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 11, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 12, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 13, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 14, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 15, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 16, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 17, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 18, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 19, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 20, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 21, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 22, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 23, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 24, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
-      {id: 25, first_name: 'test', last_name: 'asdasd', username: 'oknasirr'},
+      {id: 1, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 2, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 3, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 4, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 5, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 6, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 7, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 8, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 9, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 10, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 11, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 12, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 13, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 14, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 15, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 16, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 17, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 18, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 19, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 20, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 21, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 22, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 23, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 24, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
+      {id: 25, first_name: 'Nasir', last_name: 'Mehmood', username: 'oknasir'},
     ]
 
     setApplications(data)
   }
 
+  const testLoops = () => {
+    console.log('test loops area')
+
+    const results = applications.map((user, index) => {
+
+      // const name = user.first_name +' '+ user.last_name
+      // const name = `${user.first_name} ${user.last_name}`
+
+      user.name = `${user.first_name} ${user.last_name}`
+      
+      // console.log(name)
+
+      // if(index===5){
+      //   return user?.id
+      // }
+      return user
+    });
+    
+    // user ? user.id : null
+ /*
+    if(user) {
+      return user.id
+    } else {
+      return null
+    }
+
+
+    const numbers = [4, 9, 16, 25];
+    document.getElementById("demo").innerHTML = numbers.map((item) => {
+        return Math.sqrt(item)
+    });
+*/
+    console.log('results: ', results);
+    console.log('applications: ', applications);
+  }
+
+  /*
+  - map
+  - filter
+  - find
+  - forEach
+  - findIndex
+  - reduce
+  - reverse
+  - some
+  - sort
+  */
+
   return (
     <Container>
       <NavigationBar />
 
-      <DataTable data={applications} />
+      <Button variant="success" onClick={testLoops}>Run Loop</Button>
+
+      {results}
       
+      <DataTable data={applications} />
+
       <Pagination />
     </Container>
   );
